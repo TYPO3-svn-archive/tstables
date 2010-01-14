e64d48623d130036f6988f74a13d7ec63a0948e3
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-14 16:14:28 +0000

    Initial import (freshly kickstarted)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/tstables/trunk@28845 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "tstables"
+#
+# Auto generated 14-01-2010 17:13
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TypoScript Tables',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Fabrizio Branca',
+	'author_email' => 'fabrizio.branca@aoemedia.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"0f72";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"c8c5";s:20:"doc/wizard_form.html";s:4:"4c7e";}',
+);
+
+?>
